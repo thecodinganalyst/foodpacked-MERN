1c8b18802e6fc9b613307d7a72c21a3159436eac
Xinyi <xinyi.tay@zuhlke.com>
2021-01-15 15:28:39 +0800

    Added new API calls: retrieve, retrieveAvailable, retrieveById, update, delete

--- frontend/src/services/ListingDataService.js
+++ frontend/src/services/ListingDataService.js
@@ -3,4 +3,28 @@ import axios from "axios";
 axios.defaults.baseURL = "https://localhost:8000/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-class ListingDataService {}
+class ListingDataService {
+  create(data) {
+    return axios.post("/listings", data);
+  }
+
+  retrieve() {
+    return axios.get("/listings");
+  }
+
+  retrieveAvailable() {
+    return axios.get("/listings/available");
+  }
+
+  retrieveById(id) {
+    return axios.get(`/listings/${id}`);
+  }
+
+  update(id, data) {
+    return axios.put(`/listings/${id}`, data);
+  }
+
+  delete(id) {
+    return axios.delete(`listings/${id}`);
+  }
+}
