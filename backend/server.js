75f8403ed84ed349010511d406cd5ca302cb9906
xinyi.tay <xinyi.tay@zuhlke.com>
2021-04-09 16:22:17 +0800

    Updated port value in server.js and added .env file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
@@ -12,14 +11,11 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-// declare a public static folder. where client-side static file/output go.
-//app.use("/", express.static(_dirname + "/public"));
-
-const port = 8000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello server!" });
@@ -28,8 +24,8 @@ app.get("/", (req, res) => {
 require("./app/routes/listingRoutes.js")(app);
 
 // The app.listen() function is used to bind and listen the connections on the specified host and port.
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
 });
 
 const mongoose = require("mongoose");
