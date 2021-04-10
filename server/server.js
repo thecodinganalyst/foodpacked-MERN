1815d2e1592551889ce1e2e39da2833bbc5d01e8
Xinyi <xinzwerkz@gmail.com>
2021-04-10 13:39:18 +0800

    Updated server.js to include static path join and .gitignore to include package-lock.json

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 
 // CORS-enabled for only localhost:8081
@@ -10,10 +10,10 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse application/json
+// BodyParser middleware
 app.use(express.json());
-// parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 8080;
 
