9a9aa50f3d1dcaaf40e7f0c05d0eab7a1224953f
Xinyi <xinzwerkz@gmail.com>
2021-04-18 19:28:59 +0800

    Updated listingDataService

--- client/src/services/listingDataService.js
+++ client/src/services/listingDataService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// commented out for production
-// axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "http://localhost:8080/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 class ListingDataService {
