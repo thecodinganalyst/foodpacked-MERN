ce56d82e3a9930f8373a1e3fa493b74943da167d
Xinyi <xinzwerkz@gmail.com>
2021-04-18 22:24:26 +0800

    Updated listingDataService

--- client/src/services/listingDataService.js
+++ client/src/services/listingDataService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://foodpacked.herokuapp.com/api";
+axios.defaults.baseURL = "http://localhost:8080/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 class ListingDataService {
